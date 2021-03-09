1da9386996a7f4c62acb4561ddcdff8321724660
sugardon <sugar2don@gmail.com>
2021-03-10 00:38:56 +0900

    add global css

---
+++ nextjs-blog/pages/_app.tsx
@@ -0,0 +1,9 @@
+import {AppProps} from "next/app"
+import React from "react"
+import "../styles/global.css"
+
+export const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+    return <Component {...pageProps} />
+}
+
+export default App
