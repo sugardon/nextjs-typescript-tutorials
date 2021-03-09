bb725c304aad728d40dac6892367df89689b7dfa
sugardon <sugar2don@gmail.com>
2021-03-10 00:00:29 +0900

    add title

--- nextjs-blog/pages/posts/first-post.tsx
+++ nextjs-blog/pages/posts/first-post.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
 export const FirstPost: React.FunctionComponent = () => {
   return (
     <>
+      <Head>
+        <title> First Post </title>
+      </Head>
       <h1> FirstPost </h1>
       <h2>
         <Link href="/">
