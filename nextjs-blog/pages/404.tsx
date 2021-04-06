cb87dfae275db1e3dae290386d429f5ae315c08b
sugardon <sugar2don@gmail.com>
2021-04-07 00:42:34 +0900

    add 404

---
+++ nextjs-blog/pages/404.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+// cf. https://nextjs.org/docs/advanced-features/custom-error-page
+export const Custom404: React.FC = () => {
+  return <h1>404 - Page Not Found </h1>;
+};
+export default Custom404;
